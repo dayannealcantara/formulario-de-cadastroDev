5864d338e2761df0928e4323f8551b503e160217
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-08-18 15:47:02 -0300

    feat:creating and styling header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import * as S from './Header.styles'
+
+export const Header = () => {
+  return (
+    <S.Container>   
+      <S.TitleHeader>Cadastro de Desenvolvedor</S.TitleHeader>
+      <S.TextHeader>Faça seu cadastro na lista e receba um emprego instantaneamente.</S.TextHeader>   
+  </S.Container>
+  )
+}
