bfcd445d6a6c3a15e95b5d84dd64cd42bdffc139
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-08-18 15:48:29 -0300

    feat:creating and styling selection component

---
+++ src/components/SelectOptions/SelectOptions.tsx
@@ -0,0 +1,22 @@
+import * as S from './SelectOptions.styles'
+
+type Props={
+  title:string,
+  description:string,
+  icon:string,
+  selected:boolean,
+  onClick:() => void
+}
+const SelectOption =({title, description, icon, selected, onClick}: Props) => {
+  return(
+   <S.Container onClick={onClick} selected={selected}>
+     <S.Icon>{icon}</S.Icon>
+     <S.Info>
+       <S.Title>{title}</S.Title>
+       <S.Description>{description}</S.Description>
+     </S.Info>
+   </S.Container>
+  )
+};
+
+export default SelectOption
